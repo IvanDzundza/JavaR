6f2413271828b39673995a419c3c21dfc009d4df
in <doker2008@gmail.com>
2024-10-17 16:04:47 +0300

    new phone

---
+++ src/Iphone.java
@@ -0,0 +1,8 @@
+public class Iphone {
+    static int sum = 0;
+    static String number = "Числа";
+
+    public static void main (String[] args) {
+        System.out.println(sum + number + "1 2 3");
+    }
+}
